d7a94de0a23eede9dfb03307e26e4ad206a48b90
ivanifp <ibarlaifp@gmail.com>
2019-03-07 23:56:21 +0100

    modificado centro

--- js/cod.js
+++ js/cod.js
@@ -12,14 +12,14 @@ $(document).ready(function () {
   });
 
   $.ajax({url: "html/central.html", success: function(result){
-      $("#center").html(result);
+      maquina("center",result,200,0);
     }});    
 
   $('.home').click(function(){
     $('body').removeClass('white');
 
     $.ajax({url: "html/central.html", success: function(result){
-        $("#center").html(result);
+      maquina("center",result,200,0);
       }});
   });
 
@@ -40,5 +40,28 @@ $(document).ready(function () {
       }});
 
   });
+
+  function maquina(contenedor,texto,intervalo,n){
+    var i=0,
+     // Creamos el timer
+     timer = setInterval(function() {
+     if ( i<texto.length ) {
+      // Si NO hemos llegado al final del texto..
+      // Vamos añadiendo letra por letra y la _ al final.
+      $("#"+contenedor).html(texto.substr(0,i++) + "_");
+     } else {
+      // En caso contrario..
+      // Salimos del Timer y quitamos la barra baja (_)
+      clearInterval(timer);
+      $("#"+contenedor).html(texto);
+      // Auto invocamos la rutina n veces (0 para infinito)
+      if ( --n!=0 ) {
+       setTimeout(function() {
+        maquina(contenedor,texto,intervalo,n);
+       },3600);
+      }
+     }
+    },intervalo);
+   };
 
 });
