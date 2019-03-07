d3434849596dd0d00134c4195f9753306719424f
ivanifp <ibarlaifp@gmail.com>
2019-03-07 21:43:02 +0100

    mediaquery

--- js/cod.js
+++ js/cod.js
@@ -1,27 +1,44 @@
 $(document).ready(function () {
 
+  $("#nav2").click(function(event){
+    $("#nav3").stop().slideToggle();
+  });
+
+  $('nav').mouseover(function () { 
+    $('nav').addClass('navGrey');
+  });
+  $('nav').mouseleave(function () { 
+    $('nav').removeClass('navGrey');
+  });
+
+  $.ajax({url: "html/central.html", success: function(result){
+      $("#center").html(result);
+    }});    
+
+  $('.home').click(function(){
+    $('body').removeClass('white');
+
     $.ajax({url: "html/central.html", success: function(result){
         $("#center").html(result);
-      }});    
+      }});
+  });
 
-    $('#home').click(function(){
-        $.ajax({url: "html/central.html", success: function(result){
-            $("#center").html(result);
-          }});
-    });
+  $('.info').click(function(){
+    $('body').addClass('white');
 
-    $('#info').click(function(){
-        $.ajax({url: "html/info.html", success: function(result){
-            $("#center").show(3000).html(result);
-          }});
-    });
+    $.ajax({url: "html/info.html", success: function(result){
+        $("#center").html(result);
+      }});
+  });
 
-    $('#project').click(function () { 
-        
-        $.ajax({url: "html/projects.html", success: function(result){
-            $("#center").slideDown("slow").html(result);
-          }});
+  $('.project').click(function () { 
+      
+    $('body').removeClass('white');
 
-    });
+    $.ajax({url: "html/projects.html", success: function(result){
+        $("#center").slideDown("slow").html(result);
+      }});
+
+  });
 
 });
